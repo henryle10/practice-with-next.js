8fdc0572fe87511aa23a0abfa7efb89363c4849a
Henry Le <henryle10@gmail.com>
2021-04-06 15:44:20 -0700

    added jsconfig and messed with chakra ui, stuck on icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,39 @@
-import { ThemeProvider } from '@chakra-ui/react';
-import { AuthProvider, CSSReset } from '../lib/auth';
-import customTheme from '../styles/theme';
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
+import { Global, css } from '@emotion/react';
 
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+        html {
+          min-width: 380px;
+          scroll-behavior: smooth;
+        }
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+      `}
+      />
+      {children}
+    </>
+  );
+};
 
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={customTheme}>
       <AuthProvider>
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 }
 
